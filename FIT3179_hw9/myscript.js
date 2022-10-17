43e189749e2cf24c1d495ba1fed3fa0c0440b530
Nicolelim888 <111552605+Nicolelim888@users.noreply.github.com>
2022-10-18 04:49:28 +1100

    Add files via upload

---
+++ FIT3179_hw9/myscript.js
@@ -0,0 +1,8 @@
+var vg_1 = "airport_map.vg.json";
+vegaEmbed("#map1", vg_1).then(function(result) {}).catch(console.error);
+
+var vg_2 = "scatterplot.vg.json";
+vegaEmbed("#map2", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "across_the_years.vg.json";
+vegaEmbed("#map3", vg_3).then(function(result) {}).catch(console.error);
